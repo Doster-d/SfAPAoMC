d74c0b165c17f5ae5ad7499bb4ea9897b277d294
alexfedd <alexandrfedorov508@gmail.com>
2024-06-16 14:38:57 +0300

    Added seo tags

--- web/src/pages/fileInfo/FileInfo.jsx
+++ web/src/pages/fileInfo/FileInfo.jsx
@@ -3,22 +3,12 @@ import "./style.scss";
 import MultiRadialBar from "../../components/charts/multiRadialBar/MultiRadialBar";
 import SemiRadialBar from "../../components/charts/semiRadialBar/SemiRadialBar";
 import TablePie from "../../components/charts/tablePie/TablePie";
-import ReactApexChart from "react-apexcharts";
+import { Helmet } from "react-helmet-async";
 import SimplePie from "../../components/charts/simplePie/SimplePie";
+import { useParams } from "react-router-dom";
 function FileInfo() {
+  const {fileId} = useParams();
   const [barSelected, setBarSelected] = useState(undefined);
-  useEffect(() => {
-    console.log(barSelected);
-  }, [barSelected]);
-  const handleBarSelection = (event) => {
-    if (event.target.parentElement.attributes.seriesName) {
-      if (event.target.attributes.selected.value === "true") {
-        setBarSelected(event.target.parentElement.attributes.seriesName.value);
-      } else {
-        setBarSelected(undefined);
-      }
-    }
-  };
   useEffect(() => {
     document.body.style =
       "  background:linear-gradient(90deg, rgb(132, 250, 176), rgb(143, 211, 244) 52.465%)";
@@ -27,28 +17,51 @@ function FileInfo() {
     };
   }, []);
 
-  
+
+  const handleBarSelection = (event) => {
+    if (
+      event.target.parentElement.attributes.seriesName ||
+      event.target.attributes.selected
+    ) {
+      if (event.target.attributes.selected?.value === "true") {
+        setBarSelected(event.target.parentElement.attributes.seriesName?.value);
+      } else {
+        setBarSelected(undefined);
+      }
+    }
+  };
+
   return (
-    <main className="file-info">
-      <div className="container">
-        <h1 className="title-h1 file-info__title">
-          Статистика по файлу “наименование_файла”
-        </h1>
-        <div className="file-info__charts">
-          <div className="file-info__charts-row">
-            <MultiRadialBar
-              handleBarSelection={handleBarSelection}
-              series={[100, 90, 80]}
-            />
-            <TablePie series={[28, 12, 6]} />
-          </div>
-          <div className="file-info__charts-row">
-            <SimplePie series={[44, 55, 17, 15]}/>
-            <SemiRadialBar series={[72]} />
+    <>
+      <Helmet>
+        <title>Файл {fileId}</title>
+        <meta
+          name="description"
+          content="Описание определенного файла"
+        />
+        <meta rel="canonical" href={`/file/${fileId}`} />
+      </Helmet>
+      <main className="file-info">
+        <div className="container">
+          <h1 className="title-h1 file-info__title">
+            Статистика по файлу “наименование_файла”
+          </h1>
+          <div className="file-info__charts">
+            <div className="file-info__charts-row">
+              <MultiRadialBar
+                handleBarSelection={handleBarSelection}
+                series={[100, 90, 80]}
+              />
+              <TablePie series={[28, 12, 6]} />
+            </div>
+            <div className="file-info__charts-row">
+              <SimplePie series={[44, 55, 17, 15]} />
+              <SemiRadialBar series={[72]} />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
--- web/src/pages/mainpage/Mainpage.jsx
+++ web/src/pages/mainpage/Mainpage.jsx
@@ -4,19 +4,20 @@ import "./style.scss";
 // Import Swiper styles
 import "swiper/css";
 import TeamSlider from "../../components/teamSlider/TeamSlider";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import MultiRadialBar from "../../components/charts/multiRadialBar/MultiRadialBar";
 import SemiRadialBar from "../../components/charts/semiRadialBar/SemiRadialBar";
 import TablePie from "../../components/charts/tablePie/TablePie";
+import { Helmet } from "react-helmet-async";
 function Mainpage() {
   const [barSelected, setBarSelected] = useState(undefined);
-  useEffect(() => {
-    console.log(barSelected);
-  }, [barSelected]);
   const handleBarSelection = (event) => {
-    if (event.target.parentElement.attributes.seriesName) {
-      if (event.target.attributes.selected.value === "true") {
-        setBarSelected(event.target.parentElement.attributes.seriesName.value);
+    if (
+      event.target.parentElement.attributes.seriesName ||
+      event.target.attributes.selected
+    ) {
+      if (event.target.attributes.selected?.value === "true") {
+        setBarSelected(event.target.parentElement.attributes.seriesName?.value);
       } else {
         setBarSelected(undefined);
       }
@@ -24,31 +25,45 @@ function Mainpage() {
   };
 
   return (
-    <main className="main">
-      <section className="main-banner">
-        <div className="container">
-          <h1 className="main-banner__title title-h1">
-            Решение кейса “Сервис анализа патентной активности компаний Москвы”
-          </h1>
-          <Link className="main-banner__button">Кнопка для чего-то</Link>
-        </div>
-      </section>
-      <TeamSlider />
-      <section className="charts">
-        <h2 className="title-h2 charts__title">ОБЩАЯ СТАТИСТИКА</h2>
-        <div className="container charts__container">
-          <div className="charts__main-chart">
-            <MultiRadialBar handleBarSelection={handleBarSelection} series={[100, 90, 80]} />
+    <>
+      <Helmet>
+        <title>K1 - Анализ патентов</title>
+        <meta
+          name="description"
+          content="Сервис анализа патентной активности компаний Москвы: мониторинг, анализ и визуализация патентов."
+        />
+        <meta rel="canonical" href="/"/>
+      </Helmet>
+      <main className="main">
+        <section className="main-banner">
+          <div className="container">
+            <h1 className="main-banner__title title-h1">
+              Решение кейса “Сервис анализа патентной активности компаний
+              Москвы”
+            </h1>
+            <Link className="main-banner__button">Кнопка для чего-то</Link>
           </div>
-          {barSelected && (
-            <div className="charts__additional-charts">
-              <SemiRadialBar series={[72]} />
-              <TablePie series={[28, 12, 6]} />
+        </section>
+        <TeamSlider />
+        <section className="charts">
+          <h2 className="title-h2 charts__title">ОБЩАЯ СТАТИСТИКА</h2>
+          <div className="container charts__container">
+            <div className="charts__main-chart">
+              <MultiRadialBar
+                handleBarSelection={handleBarSelection}
+                series={[100, 90, 80]}
+              />
             </div>
-          )}
-        </div>
-      </section>
-    </main>
+            {barSelected && (
+              <div className="charts__additional-charts">
+                <SemiRadialBar series={[72]} />
+                <TablePie series={[28, 12, 6]} />
+              </div>
+            )}
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
