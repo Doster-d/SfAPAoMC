6e7c62d05c528a8bc9e8de2f69cd51bdf4ab788e
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-06-16 21:45:39 +0300

    feat: move ML modules to api/src

--- api/src/app/router.py
+++ api/src/app/router.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from fastapi import APIRouter, UploadFile
 from fastapi.responses import JSONResponse
+from src.PatentLoader.FileLoader import FileLoader
 
 app_router = APIRouter(prefix="/api")
 
@@ -13,3 +14,4 @@ async def upload_data(user_id: int, file: UploadFile) -> JSONResponse:
         fout.write(file.read())
 
     loader = FileLoader()
+    loader.load_file(str(path))
