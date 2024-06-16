e346be2718b3e018bed8cb128ab12a7790a45e25
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2024-06-16 23:38:53 +0300

    fixes

--- api/src/app/router.py
+++ api/src/app/router.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, UploadFile, Header, status, Depends
 from fastapi.responses import JSONResponse, FileResponse, Response
 
 from src.PatentLoader.FileLoader import FileLoader
+from src.CompanyCategory.OrgClassificator import OrgClassificator
 from src.auth.user_service import UserService, get_user_service
 
 from .file_service import FileService, get_file_service
@@ -80,3 +81,38 @@ async def download_data(
         )
 
     return FileResponse(file.path)
+
+
+@router.get("/information/{user_id}/{file_id}")
+async def fetch_information(
+    user_id: int,
+    file_id: int,
+    authorization: Annotated[str, Header()],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+    file_service: Annotated[FileService, Depends(get_file_service)],
+) -> JSONResponse:
+    access_token = authorization.split()[-1]
+    user = await user_service.fetch_by_token(access_token)
+
+    if user is None or user.id != user_id:
+        return JSONResponse(
+            {"detail": "Для загрузки файла нужно войти в свой аккаунт"},
+            status_code=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    file = await file_service.fetch_by_id(file_id)
+
+    if file is None:
+        return JSONResponse(
+            {"detail": "Файл не найден"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    classificator = OrgClassificator()
+    loader = FileLoader()
+    await loader.load_file(file.path)
+
+    data = loader.patent_processor.patents_ids["company_id"].to_list()
+    classificator.classify_company(data)
+
+    return classificator.classification
