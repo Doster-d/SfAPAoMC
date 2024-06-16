e8457a4f573bc9323d4b3260d1bed5af3463c3f5
alexfedd <alexandrfedorov508@gmail.com>
2024-06-16 22:27:12 +0300

    Update useUploadFileMutation.js

--- web/src/components/header/components/uploadFile/hooks/useUploadFileMutation.js
+++ web/src/components/header/components/uploadFile/hooks/useUploadFileMutation.js
@@ -4,7 +4,7 @@ import { uploadFile } from "../api/uploadFile";
 
 export function useUploadFileMutation(addNotification) {
     return useMutation({
-        mutationFn: async () => (await uploadFile(data)),
+        mutationFn: async (data) => (await uploadFile(data)),
         onError: (error) => {
             console.log(error);
             addNotification('Что-то пошло не так...', 'bad', 5000)
