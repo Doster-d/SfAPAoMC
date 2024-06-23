31902d5b5b96b990c0c411ca651e26187893fc14
alexfedd <alexandrfedorov508@gmail.com>
2024-06-23 22:56:55 +0300

    Update utils.js

--- web/src/utils.js
+++ web/src/utils.js
@@ -1,6 +1,8 @@
 export function getCookieByName(name) {
-  const cookies = document.cookie.split(";");
+  const cookies = document.cookie.split("; ");
+
   for (const cookie of cookies) {
+
     const parts = cookie.split("=");
     if (parts[0] === name) {
       return parts[1];
