f32d8c3f86e922a6b644f7999526847dc3950d94
alexfedd <alexandrfedorov508@gmail.com>
2024-06-24 01:54:45 +0300

    Update getFiles.js

--- web/src/pages/fileList/api/getFiles.js
+++ web/src/pages/fileList/api/getFiles.js
@@ -4,5 +4,5 @@ import axios from "axios";
 const getFilesInstance = axios.create({baseURL: import.meta.env.VITE_BASE_URL})
 
 export const getFiles = async ({pageParam}) => {
-    return (await getFilesInstance.get(`/files/?page=${pageParam + 1}`)).data
+    return (await getFilesInstance.get(`/files-info/?page=${pageParam + 1}`)).data
 }
